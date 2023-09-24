2996426c98f44396efc61ce6474e60283b253f7f
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-09-24 18:14:25 +0100

    init commit

--- .github/ISSUE_TEMPLATE/general.md
+++
@@ -1,7 +0,0 @@
----
-name: 🔖 General issue
-about: Any issue that is not a bug or feature request.
-title: ''
-labels: ''
-assignees: ''
----
---
+++ src/test.ts
@@ -0,0 +1,10 @@
+import ws281x from 'rpi-ws281x-native';
+
+const channel = ws281x(50, { stripType: ws281x.stripType.WS2811, gpio: 18, brightness: 255 });
+
+const colorArray = channel.array;
+for (let i = 0; i < channel.count; i++) {
+  colorArray[i] = 0xffcc22;
+}
+
+ws281x.render();
