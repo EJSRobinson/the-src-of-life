451fd9f60f28018391fb8e3abd203eab596b5b86
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-09-25 19:37:42 +0100

    Update

---
+++ src/test2.ts
@@ -0,0 +1,49 @@
+import ws281x from 'rpi-ws281x';
+
+class Example {
+    config: ws281x.Config;
+    constructor() {
+        this.config = {};
+
+        // Number of leds in my strip
+        this.config.leds = 50;
+
+        // Use DMA 10 (default 10)
+        this.config.dma = 10;
+
+        // Set full brightness, a value from 0 to 255 (default 255)
+        this.config.brightness = 255;
+
+        // Set the GPIO number to communicate with the Neopixel strip (default 18)
+        this.config.gpio = 26;
+
+        // The RGB sequence may vary on some strips. Valid values
+        // are "rgb", "rbg", "grb", "gbr", "bgr", "brg".
+        // Default is "rgb".
+        // RGBW strips are not currently supported.
+        this.config.stripType = 'grb';
+
+        // Configure ws281x
+        ws281x.configure(this.config);
+    }
+
+    run() {
+        // Create a pixel array matching the number of leds.
+        // This must be an instance of Uint32Array.
+        const pixels = new Uint32Array(this.config.leds);
+
+        // Create a fill color with red/green/blue.
+        const red = 255, green = 0, blue = 0;
+        const color = (red << 16) | (green << 8)| blue;
+
+        for (let i = 0; i < this.config.leds; i++)
+            pixels[i] = color;
+
+        // Render to strip
+        ws281x.render(pixels);
+    }
+    
+};
+
+const example = new Example();
+example.run();
