5bc709970724c789f3c6b9e72c98ffe15e1c8619
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-09-25 21:24:19 +0100

    Update

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,6 @@
 import ws281x from 'rpi-ws281x-native';
 
-const ledLength = 50;
+const ledLength = 200;
 
 const channel = ws281x(ledLength, { stripType: ws281x.stripType.WS2811, gpio: 21, brightness: 255 });
 
