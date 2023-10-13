0f68a2bba9a19987e427e9c3eb927163704d88a8
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-10-13 23:57:56 +0100

    progress

--- dist/flow.js
+++ dist/flow.js
@@ -18,7 +18,7 @@ const snakeOpts = {
 snakeOpts.seperation = Math.floor(channel.count / snakeOpts.snakes);
 const clearArray = () => {
     for (let i = 0; i < channel.count; i++) {
-        colorArray[i] = 0xff0099;
+        colorArray[i] = 0xffffff;
     }
 };
 const genSnake = (offset, array, snakeLength, reverse) => {
