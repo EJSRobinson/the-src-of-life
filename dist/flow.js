c5bea3259f0a47a4a72f2e0782f603f5c7802231
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-10-13 19:59:16 +0100

    progress

--- dist/flow.js
+++ dist/flow.js
@@ -7,19 +7,19 @@ const rpi_ws281x_native_1 = __importDefault(require("rpi-ws281x-native"));
 const rainbow_1 = require("./rainbow");
 const ledLength = 200;
 const channel = (0, rpi_ws281x_native_1.default)(ledLength, { stripType: rpi_ws281x_native_1.default.stripType.WS2811, gpio: 21, brightness: 255 });
-const snakeLength = 10;
-const colorArray = channel.array;
-const getArray = (offset) => {
+const clearArray = () => {
     for (let i = 0; i < channel.count; i++) {
         colorArray[i] = 0x000000;
     }
+};
+const genSnake = (offset, array, snakeLength) => {
     for (let i = offset; i < offset + snakeLength; i++) {
         for (let j = 0; j < snakeLength; j++) {
             let ledNumber = i + j;
             if (ledNumber > channel.count) {
                 ledNumber = ledNumber - channel.count;
             }
-            colorArray[ledNumber] = (0, rainbow_1.colorwheel)((colorOffset + i));
+            array[ledNumber] = (0, rainbow_1.colorwheel)((colorOffset + i));
         }
     }
 };
@@ -28,8 +28,11 @@ let colorOffset = 0;
 setInterval(() => {
     colorOffset = (colorOffset + 1) % 256;
 }, 1000 / 10);
+const colorArray = channel.array;
 setInterval(() => {
-    getArray(offset);
+    clearArray();
+    genSnake(offset, colorArray, 5);
+    genSnake((offset + 100) % channel.count, colorArray, 5);
     rpi_ws281x_native_1.default.render(colorArray);
     offset = (offset + 1) % channel.count;
 }, 1000 / 30);
--- src/flow.ts
+++ src/flow.ts
@@ -5,21 +5,20 @@ const ledLength = 200;
 
 const channel = ws281x(ledLength, { stripType: ws281x.stripType.WS2811, gpio: 21, brightness: 255 });
 
-const snakeLength = 10;
-
-const colorArray = channel.array;
-
-const getArray = (offset: number) => {
+const clearArray = () => {
   for (let i = 0; i < channel.count; i++) {
     colorArray[i] = 0x000000;
   }
+}
+
+const genSnake = (offset: number, array: number[], snakeLength: number) => {
   for (let i = offset; i < offset + snakeLength; i++) {
     for (let j = 0; j < snakeLength; j++) {
       let ledNumber = i + j;
       if (ledNumber > channel.count) {
         ledNumber = ledNumber - channel.count;
       }
-      colorArray[ledNumber] = colorwheel((colorOffset + i));
+      array[ledNumber] = colorwheel((colorOffset + i));
     }
   }
 }
@@ -29,8 +28,15 @@ let colorOffset = 0;
 setInterval(()=>{
   colorOffset = (colorOffset + 1) % 256;
 }, 1000 / 10)
+
+const colorArray = channel.array;
+
 setInterval(()=>{
-  getArray(offset);
+  clearArray();
+
+  genSnake(offset, colorArray, 5);
+  genSnake((offset + 100) % channel.count, colorArray, 5);
+  
   ws281x.render(colorArray);
   offset = (offset + 1) % channel.count;
 }, 1000 / 30)
