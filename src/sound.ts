5f108cc1314ab244680f3f1d5105461a90936a23
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-10-13 19:03:39 +0100

    Add basic sound processor

---
+++ src/sound.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const spawn = require('child_process').spawn;
+const proces = spawn('arecord', console.log(['-D', 'plughw:0,0', '-f', 'dat'])); // invoking the arecord command
+
+proces.on('exit', function(code, sig) { // if arecord process success
+  console.log('arecord exited with code ' + code);
+});
+
+proces.stderr.on('data', function(data) {
+    console.log("music record stderr :" + data);
+});
+
+setTimeout(function() {proces.kill('SIGTERM');
+}, 30000);
