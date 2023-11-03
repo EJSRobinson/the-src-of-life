1b4b2513529fe065cc7d451dd5cdbd431ae334a8
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-11-03 20:10:33 +0000

    progress

---
+++ src/single.ts
@@ -0,0 +1,14 @@
+import ws281x from 'rpi-ws281x-native';
+
+const ledLength = 200;
+
+const channel = ws281x(ledLength, { stripType: ws281x.stripType.WS2811, gpio: 21, brightness: 255 });
+
+
+const colorArray = channel.array;
+for (let i = 0; i < channel.count; i++) {
+  colorArray[i] = 0x000000;
+}
+
+colorArray[193] = 0xFF0000;
+ws281x.render(colorArray);
