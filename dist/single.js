0faeded87f7915cbd54406b345e23592df2cff7a
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-11-03 20:12:01 +0000

    progress

--- dist/single.js
+++ dist/single.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const rpi_ws281x_native_1 = __importDefault(require("rpi-ws281x-native"));
-const ledLength = 200;
+const ledLength = 194;
 const channel = (0, rpi_ws281x_native_1.default)(ledLength, { stripType: rpi_ws281x_native_1.default.stripType.WS2811, gpio: 21, brightness: 255 });
 const colorArray = channel.array;
 for (let i = 0; i < channel.count; i++) {
-    colorArray[i] = 0x000000;
+    colorArray[i] = 0x00FF00;
+    console.log(i);
 }
-colorArray[194] = 0xFF0000;
 rpi_ws281x_native_1.default.render(colorArray);
--- src/single.ts
+++ src/single.ts
@@ -1,14 +1,14 @@
 import ws281x from 'rpi-ws281x-native';
 
-const ledLength = 200;
+const ledLength = 194;
 
 const channel = ws281x(ledLength, { stripType: ws281x.stripType.WS2811, gpio: 21, brightness: 255 });
 
 
 const colorArray = channel.array;
 for (let i = 0; i < channel.count; i++) {
-  colorArray[i] = 0x000000;
+  colorArray[i] = 0x00FF00;
+  console.log(i);
 }
 
-colorArray[194] = 0xFF0000;
 ws281x.render(colorArray);
