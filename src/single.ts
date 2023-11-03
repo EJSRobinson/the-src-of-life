070a0948a45c6ce4890afb9794314cafbbbb914a
Elliot Robinson <elliotrobinson@Elliots-MacBook-Pro.local>
2023-11-03 20:12:42 +0000

    progress

--- src/single.ts
+++ src/single.ts
@@ -7,8 +7,10 @@ const channel = ws281x(ledLength, { stripType: ws281x.stripType.WS2811, gpio: 21
 
 const colorArray = channel.array;
 for (let i = 0; i < channel.count; i++) {
-  colorArray[i] = 0x00FF00;
-  console.log(i);
+  colorArray[i] = 0x000000;
+  if (i > 150) {
+    colorArray[i] = 0xFF0000;
+  }
 }
 
 ws281x.render(colorArray);
