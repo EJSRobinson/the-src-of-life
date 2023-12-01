d41cfaa33f1048a59dcb69d93b0a887dc02f5a3a
EJSRobinson <elliotjs.robinson@gmail.com>
2023-12-01 11:35:01 +0000

    Progress

---
+++ dist/christmas.js
@@ -0,0 +1,42 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mqtt_1 = __importDefault(require("mqtt"));
+const protocol = 'mqtts';
+const host = 'hot-bat-53.mobiusflow.io';
+const port = '8883';
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`;
+const connectUrl = `${protocol}://${host}:${port}`;
+const client = mqtt_1.default.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'mqtt',
+    password: 'supersecret',
+    reconnectPeriod: 1000,
+});
+let pingPong = null;
+client.on('connect', () => {
+    console.log('Connected to', connectUrl);
+    if (!pingPong) {
+        pingPong = setInterval(() => {
+            client.publish('tree', 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+                if (error) {
+                    console.error(error);
+                }
+            });
+        }, 5000);
+    }
+});
+client.on('message', (topic, message) => {
+    console.log('Received:', topic, message.toString());
+});
+client.on('disconnect', () => {
+    console.log('Disconnected.');
+    if (pingPong) {
+        clearInterval(pingPong);
+        pingPong = null;
+    }
+});
---
+++ src/christmas.ts
@@ -0,0 +1,45 @@
+import mqtt from 'mqtt'
+
+const protocol = 'mqtts'
+const host = 'hot-bat-53.mobiusflow.io'
+const port = '8883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `${protocol}://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+  clientId,
+  clean: true,
+  connectTimeout: 4000,
+  username: 'mqtt',
+  password: 'supersecret',
+  reconnectPeriod: 1000,
+})
+
+let pingPong: NodeJS.Timeout | null = null;
+
+client.on('connect', () => {
+  console.log('Connected to', connectUrl);
+  if (!pingPong) {
+    pingPong = setInterval(() => {
+      client.publish('tree', 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+        if (error) {
+          console.error(error)
+        }
+      })
+    }, 5000)
+  }
+})
+
+client.on('message', (topic, message) => {
+  console.log('Received:', topic, message.toString())
+})
+
+client.on('disconnect', () => {
+  console.log('Disconnected.')
+  if (pingPong) {
+    clearInterval(pingPong)
+    pingPong = null;
+  }
+})
+
