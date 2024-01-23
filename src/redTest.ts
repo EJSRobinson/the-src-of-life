b06ef45e06349c762255f8faf2fce472507921bc
EJSRobinson <elliotjs.robinson@gmail.com>
2024-01-23 20:06:23 +0000

    Progress

---
+++ src/redTest.ts
@@ -0,0 +1,15 @@
+import ws281x from 'rpi-ws281x-native';
+
+const ledLength = 200;
+
+const channel = ws281x(ledLength, {
+  stripType: ws281x.stripType.WS2811,
+  gpio: 21,
+  brightness: 255,
+});
+
+const colorArray = channel.array;
+for (let i = 0; i < channel.count; i++) {
+  colorArray[i] = 0x330000;
+}
+ws281x.render(colorArray);
