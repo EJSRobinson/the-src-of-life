7742073158e0a473db74e2bf7291176ad07e1ecc
EJSRobinson <elliotjs.robinson@gmail.com>
2024-12-13 21:20:09 +0000

    progress

--- src/misc/micro.ts
+++ src/misc/micro.ts
@@ -6,7 +6,7 @@ import { fft, util } from 'fft-js';
 
 // const file = fs.createWriteStream('output.wav', { encoding: 'binary' });
 
-const resolution = 256;
+const resolution = 512;
 const cap = 75;
 const width = 150;
 
